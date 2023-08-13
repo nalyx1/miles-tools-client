41c1b0d9e4eddfd32b2f0d4c243c55077f84c98b
João Rodrigues <joojrodrigues.dev@outlook.com>
2023-08-13 18:25:34 -0300

    feat: sign in form

---
+++ src/components/SignInForm.tsx
@@ -0,0 +1,119 @@
+"use client";
+import { useState } from "react";
+export default function LoginForm() {
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+
+  async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    console.log(user);
+  }
+
+  function handleInputChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const { name, value } = e.target;
+    setUser((prevUser) => ({
+      ...prevUser,
+      [name]: value,
+    }));
+  }
+
+  return (
+    <div className="flex flex-col w-full max-w-md px-4 py-8 bg-white rounded-lg shadow  sm:px-6 md:px-8 lg:px-10">
+      <div className="self-center mb-6 text-xl font-light text-gray-600 sm:text-2xl dark:text-white">
+        Entre com a sua conta
+      </div>
+      <div className="mt-8">
+        <form onSubmit={handleSubmit}>
+          <div className="flex flex-col mb-2">
+            <div className="flex relative">
+              <span className="rounded-l-md inline-flex  items-center px-3 border-t bg-white border-l border-b  border-gray-300 text-gray-500 shadow-sm text-sm">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75"
+                  />
+                </svg>
+              </span>
+              <input
+                type="text"
+                id="sign-in-email"
+                name="email"
+                value={user.email}
+                onChange={handleInputChange}
+                className="rounded-r-lg flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+                placeholder="E-mail"
+              />
+            </div>
+          </div>
+          <div className="flex flex-col mb-6">
+            <div className="flex relative ">
+              <span className="rounded-l-md inline-flex  items-center px-3 border-t bg-white border-l border-b  border-gray-300 text-gray-500 shadow-sm text-sm">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z"
+                  />
+                </svg>
+              </span>
+              <input
+                type="password"
+                id="sign-in-password"
+                name="password"
+                value={user.password}
+                onChange={handleInputChange}
+                className="rounded-r-lg flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
+                placeholder="Senha"
+              />
+            </div>
+          </div>
+          <div className="flex items-center mb-6 -mt-4">
+            <div className="flex ml-auto">
+              <a
+                href="#"
+                className="inline-flex text-xs font-thin text-gray-500 sm:text-sm dark:text-gray-100 hover:text-gray-700 dark:hover:text-white"
+              >
+                Esqueci minha senha
+              </a>
+            </div>
+          </div>
+          <div className="flex w-full">
+            <button
+              type="submit"
+              className="py-2 px-4  bg-purple-600 hover:bg-purple-700 focus:ring-purple-500 focus:ring-offset-purple-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg "
+            >
+              ENTRAR
+            </button>
+          </div>
+        </form>
+      </div>
+      <div className="flex items-center justify-center mt-6">
+        <a
+          href="#"
+          target="_blank"
+          className="inline-flex items-center text-xs font-thin text-center text-gray-500 hover:text-gray-700 dark:text-gray-100 dark:hover:text-white"
+        >
+          <span className="ml-2">Não tem uma conta? Registre-se</span>
+        </a>
+      </div>
+    </div>
+  );
+}
+
